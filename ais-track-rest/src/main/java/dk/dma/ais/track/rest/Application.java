e5fc365d30470327e8694834b77b373969012583
Thomas Borg Salling <tbsalling@tbsalling.dk>
2015-04-24 12:35:10 +0200

    Reversed parent from DMA to Spring - now importing DMA commons instead.

--- ais-track-rest/src/main/java/dk/dma/ais/track/rest/Application.java
+++ ais-track-rest/src/main/java/dk/dma/ais/track/rest/Application.java
@@ -17,14 +17,14 @@ package dk.dma.ais.track.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
  * @author tbsalling
  */
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @ComponentScan(basePackages = "dk.dma.ais.track")
 public class Application {
 
